daa22fe650cee4276658b945ce44367bd95d606f
CatsArePanzers <kristianm2005@gmail.com>
2021-06-25 08:45:23 +0300

    Game works somewhat

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
+import math
 import pygame
 from pygame.locals import *
 from src.classes import *
 
-SPEED = 1
-SPEED_BALL = SPEED
+speed = 1
+speed_ball = speed
 BLACK = (0, 0, 0)
 WHITE = (255, 255, 255)
 
@@ -18,46 +19,51 @@ ball.set_pos((screen_size[0] - ball.width) / 2,
     (screen_size[1] - ball.width) / 2)
 
 player_paddle = Paddle(20, 100, screen_size)
-player_paddle.set_pos(ball.width, (screen_size[1] - player_paddle.height) / 2)
+player_paddle.set_pos(60, (screen_size[1] - player_paddle.height) / 2)
 
 enemy_paddle = Paddle(20, 100, screen_size)
-enemy_paddle.set_pos(screen_size[0] - ball.width * 2, player_paddle.rect.y)
+enemy_paddle.set_pos(screen_size[0] - 80, player_paddle.rect.y)
 
 sprites = pygame.sprite.Group()
 sprites.add(player_paddle)
 sprites.add(enemy_paddle)
 sprites.add(ball)
 
-list = []
-list.append(player_paddle)
-list.append(enemy_paddle)
-list.append(ball)
+rectangles = [player_paddle, enemy_paddle, ball]
 
-#dneska ke se pravim na jasho
+is_running = True
 
-while True:
+while is_running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
-            pygame.quit()
-            exit(0)
+            is_running = False
  
-    # --- Game logic should go here
     keys = pygame.key.get_pressed()
     if keys[pygame.K_w]:
-        player_paddle.set_pos_p(player_paddle.rect.y - SPEED)
+        player_paddle.set_pos_p(player_paddle.rect.y - speed)
     elif keys[pygame.K_s]:
-        player_paddle.set_pos_p(player_paddle.rect.y + SPEED)
+        player_paddle.set_pos_p(player_paddle.rect.y + speed)
 
-    # --- Drawing code should go here
-    sprites.update()
+    if keys[pygame.K_UP]:
+        enemy_paddle.set_pos_p(enemy_paddle.rect.y - speed)
+    elif keys[pygame.K_DOWN]:
+        enemy_paddle.set_pos_p(enemy_paddle.rect.y + speed)
 
-    # First, clear the screen to black.
+    ball.move_ball(rectangles, speed)
+    if ball.rect.x > 800 or ball.rect.x < 0:
+        ball.rect.x = 400
+
     if screen_size != screen.get_size():
         screen_size = screen.get_size()
-        for i in list:
+        for i in sprites:
             i.get_screen(screen_size)
-        enemy_paddle.set_pos(screen_size[0] - ball.width * 2, screen_size[1] - enemy_paddle.rect.y)
+            i.scale()
+            speed = round(screen_size[1] / 330 - 1)
+            if speed <= 0:
+                speed = 1
+        enemy_paddle.set_pos(screen_size[0] - 80, enemy_paddle.rect.y)
 
+    sprites.update()
 
     screen.fill(BLACK)
 
--- src/classes.py
+++ src/classes.py
@@ -1,4 +1,6 @@
 import pygame
+import math
+
 BLACK = (0, 0, 0)
 WHITE = (255, 255, 255)
 
@@ -7,6 +9,7 @@ class Rectangle(pygame.sprite.Sprite):
         self.width = width
         self.height = height
         self.screen = screen
+        self.colour = colour
 
         super().__init__()
 
@@ -25,6 +28,22 @@ class Rectangle(pygame.sprite.Sprite):
     def get_screen(self, sizes):
         self.screen = sizes
 
+    def scale(self):
+        self.height = self.rect.height * self.screen[1] / 600
+        self.image = pygame.Surface([self.width, self.height])
+        self.image.fill(BLACK)
+
+        pygame.draw.rect(self.image, self.colour, [0, 0, self.width, self.height])
+
+    def are_colliding(self, other):
+        if other.rect.x <= self.rect.x + self.width:
+            if self.rect.x <= other.rect.x + other.width:
+                if other.rect.y <= self.rect.y + self.width:
+                    if self.rect.y <= other.rect.y + other.height:
+                        print("gei")
+                        return True
+                        
+
 class Paddle(Rectangle):
     def set_pos_p(self, y):
         self.set_pos(self.rect.x, y)
@@ -35,5 +54,32 @@ class Paddle(Rectangle):
             
 class Ball(Rectangle):
     def __init__(self, width):
+        self.direction_x = 1
+        self.direction_y = 1
         super().__init__(width, width)
 
+    def scale(self): 
+        self.width = self.height = self.rect.height * float(self.screen[1] / 640)
+        self.image = pygame.Surface([self.width, self.height])
+        self.image.fill(BLACK)
+
+        pygame.draw.rect(self.image, self.colour, [0, 0, self.width, self.height])
+
+    def bounce(self, other):
+        if self.are_colliding(other):
+            self.direction_x *= -1
+            self.direction_y *= -1
+        elif self.rect.y + self.width >= self.screen[1]:
+            self.direction_y = -1
+            self.rect.y -= 20
+        elif self.rect.y <= 0:
+            self.rect.y = 0
+            self.direction_y = 1
+
+    def move_ball(self, other, speed):
+        self.bounce(other[0])
+        self.bounce(other[1])
+        print(self.direction_y)
+        self.set_pos(self.rect.x + (speed) * self.direction_x,
+                     self.rect.y + (speed + 5) * self.direction_y)
+        
